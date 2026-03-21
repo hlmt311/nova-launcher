a4235eeb41cb15c2ee10acd4ea6287794ac0fe49
hlmt311 <adamjaber913@gmail.com>
2026-03-21 19:45:34 +0200

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,13 +1,24 @@
-using System.Windows;
+using System;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
 namespace NovaGamingOptimizer
 {
     public partial class App : Application
     {
-        protected override async void OnStartup(StartupEventArgs e)
+        protected override void OnStartup(StartupEventArgs e)
         {
+            DispatcherUnhandledException += (s, ex) =>
+            {
+                MessageBox.Show($"Error: {ex.Exception.Message}\n\n{ex.Exception.StackTrace}", "Nova Error");
+                ex.Handled = true;
+            };
+            AppDomain.CurrentDomain.UnhandledException += (s, ex) =>
+            {
+                MessageBox.Show($"Fatal: {ex.ExceptionObject}", "Nova Fatal Error");
+            };
             base.OnStartup(e);
-            await AutoUpdater.CheckOnStartup();
+            Task.Run(async () => await AutoUpdater.CheckOnStartup());
         }
     }
 }
